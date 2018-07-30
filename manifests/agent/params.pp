7f50f9a445228a492bb15298a09b96fea9bd9cc2
James Stremick <james.stremick@vonage.com>
2018-07-30 10:10:00 -0400

    iterating

--- manifests/agent/params.pp
+++ manifests/agent/params.pp
@@ -4,10 +4,10 @@
 # It sets variables according to platform.
 #
 class vault::agent::params {
-  $user               = 'vault'
-  $manage_user        = true
-  $group              = 'vault'
-  $manage_group       = true
+  $user               = $::vault::user
+  $manage_user        = $::vault::manage_user
+  $group              = $::vault::group
+  $manage_group       = $::vault::manage_group
   $service_name       = 'vault-agent'
 
   # These should always be undef as they are optional settings that
